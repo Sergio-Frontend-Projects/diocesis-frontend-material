09374401524d5ce7d10c0bc25ffb51edbeb5e665
Sergio Barreras <sergiobg.isc@gmail.com>
2025-03-22 14:05:42 -0700

    fuentes y logos

--- src/app/layout/admin-layout/admin-layout.component.ts
+++ src/app/layout/admin-layout/admin-layout.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -7,6 +7,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../../core/auth.service';
+
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-admin-layout',
@@ -26,8 +28,29 @@ export class AdminLayoutComponent {
   auth = inject(AuthService);
   router = inject(Router);
 
+  breakpointObserver = inject(BreakpointObserver);
+
+  isSmallScreen = signal(false);
+  sidenavOpened = signal(true);
+
+  constructor() {
+    effect(() => {
+      this.breakpointObserver
+        .observe([Breakpoints.Handset])
+        .subscribe((result) => {
+          const isSmall = result.matches;
+          this.isSmallScreen.set(isSmall);
+          this.sidenavOpened.set(!isSmall);
+        });
+    });
+  }
+
   logout() {
     this.auth.logout();
     this.router.navigateByUrl('/login');
+  }
+
+  toggleSidenav() {
+    this.sidenavOpened.update((open) => !open);
   }
 }
