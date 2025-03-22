7ee18592c0719403d5ffb2f6ab64518d6b3dfa3d
Sergio Barreras <sergiobg.isc@gmail.com>
2025-03-22 13:03:46 -0700

    estilo de layout

--- src/app/layout/admin-layout/admin-layout.component.ts
+++ src/app/layout/admin-layout/admin-layout.component.ts
@@ -1,22 +1,33 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { AuthService } from '../../core/auth.service';
 
 @Component({
   selector: 'app-admin-layout',
   imports: [
     CommonModule,
-    RouterModule,
-    MatSidenavModule,
+    RouterOutlet,
     MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
     MatIconModule,
     MatListModule,
   ],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.scss',
 })
-export class AdminLayoutComponent {}
+export class AdminLayoutComponent {
+  auth = inject(AuthService);
+  router = inject(Router);
+
+  logout() {
+    this.auth.logout();
+    this.router.navigateByUrl('/login');
+  }
+}
