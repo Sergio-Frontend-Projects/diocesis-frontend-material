1cca939d5cf6f755b97322139aef649e3ca1ed24
Sergio Barreras <sergiobg.isc@gmail.com>
2025-04-04 00:54:37 -0700

    create: ayuda form

--- src/app/public/components/ayuda-form/ayuda-form.component.ts
+++ src/app/public/components/ayuda-form/ayuda-form.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-ayuda-form',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './ayuda-form.component.html',
-  styleUrl: './ayuda-form.component.scss'
+  styleUrl: './ayuda-form.component.scss',
 })
 export class AyudaFormComponent {
+  fb = inject(FormBuilder);
 
+  form = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+  });
+
+  onSubmit() {
+    if (this.form.invalid) return;
+
+    const formData = this.form.value;
+    console.log('Enviar al backend:', formData);
+  }
 }
